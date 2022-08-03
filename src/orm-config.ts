983522984ed8303e2267461e5892fc5101f5e481
shinypeterhan <shinypeterhan@gmail.com>
2022-08-03 13:10:24 -0400

    first commit

---
+++ src/orm-config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+
+export const ORM_CONFIG: TypeOrmModuleOptions = {
+  keepConnectionAlive: true,
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'password',
+  database: 'nest',
+  entities: [User],
+  synchronize: true,
+  logging: false,
+};
